ef04cddd557c08b00c0ecb225eee190920b87cdc
Bid Adieu <todd@bidadi.eu>
2010-11-20 01:47:01 -0600

    Fixed routes for nav links to use X_path. And added a logo helper.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ SampleApp::Application.routes.draw do
     match '/contact', :to => 'pages#contact'
     match '/about', :to => 'pages#about'
     match '/help', :to => 'pages#help'
-    get "pages/home"
 
 # The priority is based upon order of creation:
 # first created -> highest priority.
