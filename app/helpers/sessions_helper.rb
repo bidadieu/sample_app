41829ae298b4df262804063e23709ed50db1a6da
Bid Adieu <todd@bidadi.eu>
2010-11-24 17:56:12 -0600

    Completed signin/out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,8 +2,18 @@ module SessionsHelper
 
   def sign_in(user)
     # puts a cookie in user's browser; user.id signed with salt
+    # keeps a user permanently-ish logged in
     cookies.permanent.signed[:remember_token] = [user.id, user.salt]
     current_user = user
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def sign_out
+    cookies.delete(:remember_token)
+    current_user = nil
   end
 
   def current_user=(user)
@@ -14,9 +24,6 @@ module SessionsHelper
     @current_user ||= user_from_remember_token
   end
 
-  def signed_in?
-    !current_user.nil?
-  end
 
   private
   
