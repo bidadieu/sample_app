11de40799354360828d872d2df24e14879d82b17
Bid Adieu <todd@bidadi.eu>
2010-11-28 02:57:08 -0600

    Made a working feed using an SQL subselect and SCOPE

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -22,8 +22,20 @@ class Micropost < ActiveRecord::Base
 
   default_scope :order => "microposts.created_at DESC"
 
-  def self.from_users_followed_by(user)
-    followed_ids = user.following.map(&:id).join(", ")
-    where("user_id IN (#{followed_ids}) OR user_id = ?", user)
+  #scope: allows us to chain methods
+  scope :from_users_followed_by, lambda { |user| followed_by(user) }
+
+  private
+
+  def self.followed_by(user)
+    # NOTE: the following line is replaced by the subsequent
+    #   SQL subselect, which allows for the followed_ids to
+    #   be assembled at the DB level --> quicker/less memory
+    # followed_ids = user.following.map(&:id).join(", ") 
+    followed_ids = %(SELECT followed_id FROM relationships
+                     where follower_id = :user_id)
+    where("user_id IN (#{followed_ids}) OR user_id = :user_id", 
+          :user_id => user)
   end
+
 end
