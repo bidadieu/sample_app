41da9a040bb25626f5f0df6a006a4bf192ae1fb9
Bid Adieu <todd@bidadi.eu>
2010-11-27 21:13:31 -0600

    Finished microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -193,8 +193,25 @@ describe User do
         end.should raise_error(ActiveRecord::RecordNotFound)
       end
     end
+
+    describe "status feed" do
+      it "should have a feed" do
+        @user.should respond_to(:feed)
+      end
+
+      it "should include the user's microposts" do
+        @user.feed.should include(@mp1)
+        @user.feed.should include(@mp2)
+      end
+
+      it "should not include a different user's microposts" do
+        diff_user = Factory(:user, :email => Factory.next(:email))
+        mp3 = Factory(:micropost, :user => diff_user)
+        @user.feed.should_not include(mp3)
+      end
+    end
+
   end
-  
 end
 
 
